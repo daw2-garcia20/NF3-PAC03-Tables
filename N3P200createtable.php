17ce261513bbefa2c2d7f5789b616d480d6b63d8
daw2-garcia20 <55799101+daw2-garcia20@users.noreply.github.com>
2019-11-17 19:44:19 +0100

    Update N3P200createtable.php

--- N3P200createtable.php
+++ N3P200createtable.php
@@ -1,16 +1,9 @@
 <?php
-//connect to MySQL
 $db = mysqli_connect('localhost', 'root') or 
     die ('Unable to connect. Check your connection parameters.');
-
-//create the main database if it doesn't already exist
 $query = 'CREATE DATABASE IF NOT EXISTS reviews';
 mysqli_query($db,$query) or die(mysqli_error($db));
-
-//make sure our recently created database is the active one
 mysqli_select_db($db,'reviews') or die(mysqli_error($db));
-
-//create the movie table
 $query = 'CREATE TABLE comic (
         id_comic        INTEGER UNSIGNED  NOT NULL AUTO_INCREMENT, 
         nombre_comic      VARCHAR(255)      NOT NULL, 
@@ -24,8 +17,6 @@ $query = 'CREATE TABLE comic (
     ) 
     ENGINE=MyISAM';
 mysqli_query($db,$query) or die (mysqli_error($db));
-
-//create the movietype table
 $query = 'CREATE TABLE tipocomic ( 
         tipocomic_id    TINYINT UNSIGNED NOT NULL AUTO_INCREMENT, 
         tipocomic_label VARCHAR(100)     NOT NULL, 
@@ -33,18 +24,14 @@ $query = 'CREATE TABLE tipocomic (
     ) 
     ENGINE=MyISAM';
 mysqli_query($db,$query) or die(mysqli_error($db));
-
-//create the people table
 $query = 'CREATE TABLE cliente ( 
         cliente_id         INTEGER UNSIGNED    NOT NULL AUTO_INCREMENT, 
         cliente_fullname   VARCHAR(255)        NOT NULL, 
         cliente_isman    TINYINT(1) UNSIGNED NOT NULL DEFAULT 0, 
         cliente_iswoman TINYINT(1) UNSIGNED NOT NULL DEFAULT 0, 
-
         PRIMARY KEY (cliente_id)
     ) 
     ENGINE=MyISAM';
 mysqli_query($db,$query) or die(mysqli_error($db));
-
 echo 'La Base de datos reviews ha sido creada correctamente';
 ?>
